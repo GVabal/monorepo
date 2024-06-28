fe8a16fac560b12690b37d11f4f62a14e65033ae
Gabrielius Vabalas <gabrielius.vabalas@sodra.lt>
2024-06-28 09:48:32 +0300

    playing around with JavaFX

--- src/main/java/dev/vabalas/matas/ui/Controller.java
+++ src/main/java/dev/vabalas/matas/ui/Controller.java
@@ -1,4 +1,22 @@
 package dev.vabalas.matas.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+
+    @FXML
+    private ImageView racoonImage;
+
+    @FXML
+    private ToggleButton showHideButton;
+
+    @FXML
+    void toggleRacoonImage(ActionEvent event) {
+        boolean imageVisible = racoonImage.isVisible();
+        showHideButton.setText(imageVisible ? "Show" : "Hide");
+        racoonImage.setVisible(!imageVisible);
+    }
 }
